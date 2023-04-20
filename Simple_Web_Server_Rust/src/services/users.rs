f1ff478b2c18af0451867aeafc03d8c9772f04bf
V.O.T <makluganteng11@gmail.com>
2023-04-20 23:13:40 +0700

    fixed connection to db.

--- Simple_Web_Server_Rust/src/services/users.rs
+++ Simple_Web_Server_Rust/src/services/users.rs
@@ -1,18 +1,17 @@
 use Simple_Web_Server_Rust::{establish_connection, schema::users};
-use diesel::Connection;
-use diesel::RunQueryDsl;
+use diesel::{Connection, PgConnection, RunQueryDsl};
 use rocket::serde::json::Value;
 use rocket::serde::json::serde_json::json;
 use crate::models::models::UserInputUser;
 use crate::models::models::User;
 
 pub fn create_user_input(user_info: &UserInputUser) -> Value {
-    let connection = establish_connection();
+    let mut connection = establish_connection();
     let new_user = User {
         email: &user_info.email,
         password: &user_info.password,
     };
-    let result = diesel::insert_into(users::table).values(&new_user).execute(connection);
+    let result = diesel::insert_into(users::table).values(&new_user).execute(&mut connection);
     match result {
         Ok(user) => json!({
             "status": "success",
